89c09cd13d0313a1ad43600fab44ab72c9dff34b
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2016-06-01 10:47:08 -0400

    minor updates

--- read_data.py
+++ read_data.py
@@ -2,18 +2,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Load the data
 # See http://docs.scipy.org/doc/numpy/user/basics.io.genfromtxt.html
 data = np.genfromtxt('text_data.txt', delimiter=',')
-print(data.shape)  # examine data shape
 
+# Examine data
+print(data.shape)
 plt.plot(data[:, 0], data[:, 1], color='blue', marker='o', linestyle='')
 
 
 # Fit with Numpy
-xdata = np.sort(data[:, 0])  # for convenience in plotting
-
 coeff = np.polyfit(data[:, 0], data[:, 1], 2)
 print(coeff)  # coefficients of the fit
+
+xdata = np.sort(data[:, 0])  # for convenience in plotting
 plt.plot(xdata, coeff[2] + coeff[1] * xdata + coeff[0] * xdata**2, color='red', marker='', linestyle='-')
 
 
@@ -21,12 +23,12 @@ plt.plot(xdata, coeff[2] + coeff[1] * xdata + coeff[0] * xdata**2, color='red', 
 # See http://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html#scipy.optimize.curve_fit
 from scipy.optimize import curve_fit
 
-
+# Write a function for scipy
 def myfunction(x, a, b, c):
     return a + b * x + c * x**2
 
-
 res, cov = curve_fit(myfunction, data[:, 0], data[:, 1])
+
 print(res)  # Best fit parameters
 print(cov)  # Covariance of parameters
 perr = np.sqrt(np.diag(cov))
